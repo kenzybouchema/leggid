74d5c2fd35c6fdebf1b71b706d01a0e690713fb7
Kenzy B <kenzy.bouchema@gmail.com>
2020-11-28 18:49:12 +0100

    Leggid - Init commit: A reddit clone with Spring Web Lombok, Spring Security, Spring Data JPA, My SQL Java Driver and Java Mail Sender

---
+++ src/main/java/com/kb/leggid/LeggidApplication.java
@@ -0,0 +1,13 @@
+package com.kb.leggid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeggidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeggidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kb/leggid/LeggidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kb.leggid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeggidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
