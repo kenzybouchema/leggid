13dee20e6fd817018b3f261374bafce6ff973a3f
Kenzy B <kenzy.bouchema@gmail.com>
2020-11-28 21:47:19 +0100

    Leggid - Ajouts des Repositories

---
+++ src/main/java/com/kb/leggid/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.kb.leggid.repository;
+
+import com.kb.leggid.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
---
+++ src/main/java/com/kb/leggid/repository/VerificationTokenRepository.java
@@ -0,0 +1,9 @@
+package com.kb.leggid.repository;
+
+import com.kb.leggid.model.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken,Long> {
+}
