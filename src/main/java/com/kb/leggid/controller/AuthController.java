6cbe4be067ccc89adcfce00dbc9337f27da2662d
Kenzy B <kenzy.bouchema@gmail.com>
2020-11-29 18:40:48 +0100

    Leggid - Completion de la méthode d'enregistrement d'un utilisateur et ajout de la méthode de vérification d'un utilisateur dans le controller

--- src/main/java/com/kb/leggid/controller/AuthController.java
+++ src/main/java/com/kb/leggid/controller/AuthController.java
@@ -1,17 +1,29 @@
 package com.kb.leggid.controller;
 
 import com.kb.leggid.dto.RegisterRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kb.leggid.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/api/auth")
+@AllArgsConstructor
 public class AuthController {
 
-    @PostMapping("/signup")
-    public void signup(@RequestBody RegisterRequest registerRequest) {
+    private final AuthService authService;
 
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
+        authService.signup(registerRequest);
+        return new ResponseEntity<>("User Registration Successful", OK);
+    }
+
+    @GetMapping("accountVerification/{token}")
+    public ResponseEntity<String> verifyAccount(@PathVariable String token) {
+        authService.verifyAccount(token);
+        return new ResponseEntity<>("Account Activated Successfully", OK);
     }
 }
