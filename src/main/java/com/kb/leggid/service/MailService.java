f40b6bdba2a749ed57ac10f79d835c1062b88743
Kenzy B <kenzy.bouchema@gmail.com>
2020-11-29 17:25:50 +0100

    Leggid - Service d'envoi d'email et model de la notification de l'email

---
+++ src/main/java/com/kb/leggid/service/MailService.java
@@ -0,0 +1,34 @@
+package com.kb.leggid.service;
+
+import com.kb.leggid.model.NotificationEmail;
+import lombok.AllArgsConstructor;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+class MailService {
+
+    private final JavaMailSender mailSender;
+    private final MailContentBuilder mailContentBuilder;
+
+    void sendMail(NotificationEmail notificationEmail) {
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("springreddit@email.com");
+            messageHelper.setTo(notificationEmail.getRecipient());
+            messageHelper.setSubject(notificationEmail.getSubject());
+            messageHelper.setText(notificationEmail.getBody());
+        };
+        try {
+            mailSender.send(messagePreparator);
+
+        } catch (MailException e) {
+
+        }
+    }
+
+}
