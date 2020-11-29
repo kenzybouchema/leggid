24ce0649f764ab76bc8c342dda4e23e4858e49e2
Kenzy B <kenzy.bouchema@gmail.com>
2020-11-29 15:17:40 +0100

    Leggid - Utilisation du repository pour ecrire en base de données le user enregistré

--- src/main/java/com/kb/leggid/service/AuthService.java
+++ src/main/java/com/kb/leggid/service/AuthService.java
@@ -4,8 +4,6 @@ import com.kb.leggid.dto.RegisterRequest;
 import com.kb.leggid.model.User;
 import com.kb.leggid.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -30,6 +28,6 @@ public class AuthService {
         user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         user.setCreated(Instant.now());
         user.setEnabled(false);// Tant que l'utilisateur n'est pas validé par m
-
+        userRepository.save(user);
     }
 }
