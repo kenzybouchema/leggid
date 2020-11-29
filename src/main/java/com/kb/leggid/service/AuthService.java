4fcb96b61f83ad213f51ba354da9a116dc31badb
Kenzy B <kenzy.bouchema@gmail.com>
2020-11-29 14:56:14 +0100

    Leggid - Service d'authentification, DTO -> POJO

---
+++ src/main/java/com/kb/leggid/service/AuthService.java
@@ -0,0 +1,20 @@
+package com.kb.leggid.service;
+
+import com.kb.leggid.dto.RegisterRequest;
+import com.kb.leggid.model.User;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+public class AuthService {
+    
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(registerRequest.getPassword());
+        user.setCreated(Instant.now());
+        user.setEnabled(false); // Tant que l'utilisateur n'est pas 
+    }
+}
