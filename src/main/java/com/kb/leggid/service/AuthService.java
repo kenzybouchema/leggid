97e113df484f6e25c25fc925f454b4d808b66946
Kenzy B <kenzy.bouchema@gmail.com>
2020-11-29 15:42:42 +0100

    Leggid - Crée un token pour l'utilisateur enregistre le couple dans la base de données renvoie le token qui doit être envoyé par mail par la suite

--- src/main/java/com/kb/leggid/service/AuthService.java
+++ src/main/java/com/kb/leggid/service/AuthService.java
@@ -2,13 +2,16 @@ package com.kb.leggid.service;
 
 import com.kb.leggid.dto.RegisterRequest;
 import com.kb.leggid.model.User;
+import com.kb.leggid.model.VerificationToken;
 import com.kb.leggid.repository.UserRepository;
+import com.kb.leggid.repository.VerificationTokenRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor // Lombok génère le constructeur avec tout les args
@@ -21,6 +24,8 @@ public class AuthService {
 
     private final UserRepository userRepository;
 
+    private final VerificationTokenRepository verificationTokenRepository;
+
     @Transactional
     // Si au niveau de la classe toute les émthodes sont transactionnelles,// ici on préfère le niveau méthode
     public void signup(RegisterRequest registerRequest) {
@@ -31,5 +36,23 @@ public class AuthService {
         user.setCreated(Instant.now());
         user.setEnabled(false);// Tant que l'utilisateur n'est pas validé par m
         userRepository.save(user);
+
+        String token = generateVerificationToken(user);
+
+    }
+
+    /**
+     * Crée un token pour l'utilisateur enregistre le couple dans la base de données
+     *  renvoie le token qui doit être envoyé par mail par la suite
+     * @param user
+     * @return
+     */
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+        verificationTokenRepository.save(verificationToken);
+        return token;
     }
 }
