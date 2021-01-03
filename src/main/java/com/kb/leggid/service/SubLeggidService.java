137e19f27bf1ab3ddb3959489fa11ff3b32d56d7
Kenzy B <kenzy.bouchema@gmail.com>
2021-01-03 20:58:35 +0100

    Leggid - Remplacement des méthodes de mapping par l'injection du mapper

--- src/main/java/com/kb/leggid/service/SubLeggidService.java
+++ src/main/java/com/kb/leggid/service/SubLeggidService.java
@@ -2,6 +2,7 @@ package com.kb.leggid.service;
 
 import com.kb.leggid.dto.SubLeggidDto;
 import com.kb.leggid.exceptions.SpringRedditException;
+import com.kb.leggid.mapper.SubLeggidMapper;
 import com.kb.leggid.model.SubLeggid;
 import com.kb.leggid.repository.SubLeggidRepository;
 import lombok.AllArgsConstructor;
@@ -20,38 +21,24 @@ public class SubLeggidService {
 
     private final SubLeggidRepository SubleggidRepository;
 
+    private final SubLeggidMapper subLeggidMapper;
+
     @Transactional
     public SubLeggidDto save(SubLeggidDto subLeggidDto) {
-        SubLeggid save = SubleggidRepository.save(mapToSubleggid(subLeggidDto));
+        SubLeggid save = SubleggidRepository.save(subLeggidMapper.mapDtoToSubreddit(subLeggidDto));
         subLeggidDto.setId(save.getId());
         return subLeggidDto;
     }
 
-    private SubLeggid mapToSubleggid(SubLeggidDto subLeggidDto) {
-        return SubLeggid.builder()
-                .name(subLeggidDto.getName())
-                .description(subLeggidDto.getDescription())
-                .build();
-    }
-
-
     @Transactional(readOnly = true)
     public List<SubLeggidDto> getAll() {
-        return SubleggidRepository.findAll().stream().map(this::mapToDto).collect(toList());
-    }
-
-    private SubLeggidDto mapToDto(SubLeggid subLeggid) {
-        return SubLeggidDto.builder()
-                .id(subLeggid.getId())
-                .name(subLeggid.getName())
-                .description(subLeggid.getDescription())
-                .build();
+        return SubleggidRepository.findAll().stream().map(subLeggidMapper::mapSubredditToDto).collect(toList());
     }
 
     @Transactional(readOnly = true)
      public SubLeggidDto getSubleggid(Long id) {
          SubLeggid subLeggid = SubleggidRepository.findById(id)
                  .orElseThrow(() -> new SpringRedditException("No Subleggid found with ID - " + id));
-         return mapToDto(subLeggid);
+         return subLeggidMapper.mapSubredditToDto(subLeggid);
      }
 }
