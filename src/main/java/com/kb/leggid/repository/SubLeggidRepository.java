ef82aab2af88b0bd38388fad9d01a0ff104e976c
Kenzy B <kenzy.bouchema@gmail.com>
2021-01-03 21:40:34 +0100

    Leggid - Ajout d'une méthode de lecture des sous topics par nom

--- src/main/java/com/kb/leggid/repository/SubLeggidRepository.java
+++ src/main/java/com/kb/leggid/repository/SubLeggidRepository.java
@@ -4,6 +4,9 @@ import com.kb.leggid.model.SubLeggid;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SubLeggidRepository  extends JpaRepository<SubLeggid, Long> {
+    Optional<SubLeggid> findByName(String subredditName);
 }
